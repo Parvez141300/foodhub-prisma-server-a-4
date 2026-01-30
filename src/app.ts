fe95d6dbced0035ce1fac67e7453654f0e2af63c
Parvez141300 <parvezhossain744471@gmail.com>
2026-01-30 22:12:19 +0600

    created a category route file to post a category

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import { mealRouter } from "./modules/meal/meal.route";
+import { categoryRouter } from "./modules/category/category.route";
 
 export const app = express();
 app.all("/api/auth/*splat", toNodeHandler(auth));
@@ -10,7 +11,8 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", mealRouter)
+app.use("/api", mealRouter);
+app.use("/api", categoryRouter);
 
 app.get('/', (req, res) => {
   res.send('FoodHub server is running');

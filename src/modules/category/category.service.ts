40e012290cdda5016646fb7b0471791228f23ab9
Parvez141300 <parvezhossain744471@gmail.com>
2026-01-30 22:07:11 +0600

    created a category service file to create a category into db

---
+++ src/modules/category/category.service.ts
@@ -0,0 +1,14 @@
+import { Category } from "../../../generated/prisma/client";
+import { prisma } from "../../lib/prisma"
+
+const createCategoryIntoDB = async (payload: Category) => {
+    console.log(payload);
+    const result = await prisma.category.create({
+        data: payload,
+    });
+    return result;
+}
+
+export const mealService = {
+    createCategoryIntoDB,
+}

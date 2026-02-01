010de0961cc3cfacd8a1f5bfeb96ff9fe09b4cbc
Parvez141300 <parvezhossain744471@gmail.com>
2026-02-01 21:19:51 +0600

    used the auth middleware in review api route

--- src/modules/review/review.route.ts
+++ src/modules/review/review.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { reviewController } from "./review.controller";
+import authMiddleware, { UserRole } from "../../middleware/authMiddleware";
 
 const router = Router();
 
 router.get("/reviews/:mealId", reviewController.getAllReviewByMealId);
-router.post("/reviews", reviewController.createReview);
+router.post("/reviews", authMiddleware(UserRole.CUSTOMER, UserRole.PROVIDER, UserRole.ADMIN), reviewController.createReview);
 
 export const reviewRouter = router;

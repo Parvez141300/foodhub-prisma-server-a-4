b0768b88588a5349260b5b262b7ce584a24dd83e
Parvez141300 <parvezhossain744471@gmail.com>
2026-02-01 21:24:08 +0600

    used auth middleware in user api route like get all or search user and user status update api route

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import authMiddleware, { UserRole } from "../../middleware/authMiddleware";
 
 const router = Router();
 
 router.get("/providers", userController.getAllProvider);
 router.get("/providers/:providerId", userController.getProviderWithMenu);
-router.get("/admin/users", userController.getAllOrSearchUser);
-router.patch("/admin/users/:userId", userController.updateUserStatus);
+router.get("/admin/users", authMiddleware(UserRole.ADMIN), userController.getAllOrSearchUser);
+router.patch("/admin/users/:userId", authMiddleware(UserRole.ADMIN), userController.updateUserStatus);
 
 export const userRouter = router

9c4cf94ce887764933f29de104adeca773691515
Parvez141300 <parvezhossain744471@gmail.com>
2026-02-16 22:06:40 +0600

    modified the paginationAndSortignHelper fuction

--- src/helpers/paginationAndSortingHelper.ts
+++ src/helpers/paginationAndSortingHelper.ts
@@ -9,9 +9,9 @@ type PaginationAndSortingOptions = {
 
 
 export const paginationAndSortingHelper = (options: PaginationAndSortingOptions) => {
-    const page = Number(options.page);
-    const limit = Number(options.limit);
-    const skip = (page - 1) * limit;
+    const page = Number(options.page) || 1;
+    const limit = Number(options.limit) || 10;
+    const skip = (page - 1) * limit || 0;
     const sort_by = options.sort_by || "created_at";
     const sort_order = options.sort_order || "desc";
 

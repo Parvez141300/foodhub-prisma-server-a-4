74bede5095552b880a5fc10092d323b3d3f67886
Parvez141300 <parvezhossain744471@gmail.com>
2026-02-23 00:52:17 +0600

    edited the meal update service

--- src/modules/meal/meal.service.ts
+++ src/modules/meal/meal.service.ts
@@ -95,7 +95,17 @@ const createMealIntoDB = async (payload: Meal) => {
     return result;
 }
 
-const updateMealByIdInDB = async ({ mealId, payload }: { mealId: string, payload: Meal}) => {
+const updateMealByIdInDB = async ({ mealId, payload }: { mealId: string, payload: {
+    title: string,
+    description: string,
+    image_url: string,
+    price: string,
+    stock: string,
+    is_available: string,
+    category_id: string,
+    cuisine_id: string,
+    dietery_id: string,
+}}) => {
     const mealData = await prisma.meal.findUnique({
         where: {
             id: mealId
@@ -103,10 +113,14 @@ const updateMealByIdInDB = async ({ mealId, payload }: { mealId: string, payload
         select: {
             id: true,
             provider_id: true,
+            category_id: true,
+            cuisine_id: true,
+            dietery_id: true,
         }
     });
     if (!mealData?.id) {
-        return { message: "To update Meal this meal does not exist" };
+        throw new Error("To update Meal this meal does not exist");
+        return;
     }
 
     const providerData = await prisma.user.findUnique({
@@ -118,28 +132,43 @@ const updateMealByIdInDB = async ({ mealId, payload }: { mealId: string, payload
         }
     });
     if (providerData?.id !== mealData.provider_id) {
-        return { message: "creator is not valid to update meal's data" };
+        throw new Error("creator is not valid to update meal's data");
+        return;
     }
+
+    await prisma.category.findUniqueOrThrow({
+        where: {
+            id: payload.category_id,
+        }
+    });
+    await prisma.cuisine.findUniqueOrThrow({
+        where: {
+            id: payload.cuisine_id,
+        }
+    });
+    await prisma.dietery.findUniqueOrThrow({
+        where: {
+            id: payload.dietery_id,
+        }
+    });
 
     const result = await prisma.meal.update({
         where: {
             id: mealId
         },
         data: {
-            ...payload,
+            title: payload.title,
+            description: payload.description,
+            image_url: payload.image_url,
+            stock: Number(payload?.stock),
             price: Number(payload?.price),
+            is_available: Boolean(payload?.is_available),
+            category: {connect: { id: payload.category_id }},
+            cuisine: {connect: { id: payload.cuisine_id }},
+            dietery: {connect: { id: payload.dietery_id }},
         },
-        select: {
-            id: true,
-            title: true,
-            category: {
-                select: {
-                    id: true,
-                    name: true,
-                }
-            },
-        }
     });
+
     return result;
 }
 

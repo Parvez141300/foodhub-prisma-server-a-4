3030f067bef659a2d79411a6eea12489e1852765
Parvez141300 <parvezhossain744471@gmail.com>
2026-02-28 13:48:16 +0600

    modified the profile service update profile api route

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -19,8 +19,38 @@ const getUserProfileFromDB = async (userId: string) => {
     }
 }
 
-const updateOrInsertProfileInDB = async (paylaod: Profile) => {
-    const { id, created_at, updated_at, user_id, ...rest_data } = paylaod;
+// {
+//     "name": "Parvez Hossain Alif",
+//     "email": "parvezhossain744471@gmail.com",
+//     "phone": "01743063201",
+//     "division": "Dhaka",
+//     "district": "Dhaka",
+//     "thana": "Tongi",
+//     "area": "Ershad Nagar",
+//     "street": "Ershad Nagar, Tongi, Gazipur City Corporation",
+//     "postal_code": "1712",
+//     "user_id": "5JQsuHRa3rwbeWHaannN8EJmSUd01uMH",
+//     "image": ""
+// }
+
+type UpdateProfileType = {
+    user_id: string;
+    name: string;
+    image?: string;
+    email: string;
+
+    phone: string;
+
+    division: string;
+    district: string;
+    thana: string;
+    area: string;
+    street?: string;
+    postal_code?: string;
+}
+
+const updateOrInsertProfileInDB = async (paylaod: UpdateProfileType) => {
+    const { user_id, name, image='', email, phone, division, district, thana, area, street='', postal_code='' } = paylaod;
 
     const userData = await prisma.user.findUnique({
         where: {
@@ -31,16 +61,40 @@ const updateOrInsertProfileInDB = async (paylaod: Profile) => {
         return { message: "User not found" };
     }
 
+    await prisma.user.update({
+        where: {
+            id: user_id
+        },
+        data: {
+            name,
+            email,
+            phone,
+            image
+        }
+    })
+
     const result = await prisma.profile.upsert({
         where: {
             user_id: user_id,
         },
         update: {
-            ...rest_data,
+            phone,
+            division,
+            district,
+            thana,
+            area,
+            street,
+            postal_code
         },
         create: {
             user_id: user_id,
-            ...rest_data,
+            phone,
+            division,
+            district,
+            thana,
+            area,
+            street,
+            postal_code
         }
     });
 
